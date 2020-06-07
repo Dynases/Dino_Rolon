09de69505cf8ddfefbaeebd1cca9828da1e5e3cb
Diego <dcayo.156@gmail.com>
2020-06-06 21:32:13 -0400

    Fix Program

--- PRESENTER/Program.cs
+++ PRESENTER/Program.cs
@@ -16,7 +16,7 @@ namespace PRESENTER
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new com.Reporte.F2_CompraIngreso());
+            Application.Run(new adm.Login());
         }
     }
 }
